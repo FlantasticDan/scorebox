8fa39d948d990dc44f42a06bd174b01e0e6da2ef
Daniel Flanagan <dancharley8@gmail.com>
2021-01-23 20:35:07 -0800

    Cleanslate

--- instructions.py
+++
@@ -1,7 +0,0 @@
-'''Instructions for QLabels'''
-# pylint: skip-file
-
-corner_selection = '''\
-    Starting with the top left corner, click each corner of the scoreboard
-    in a clockwise fashion.
-'''.replace('\n    ', ' ')
